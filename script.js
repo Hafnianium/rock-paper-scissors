284ce463b9a33c1bc93761c6145250e0a122e081
patrick <patrick@o-neill.ca>
2021-10-28 17:36:19 -0400

    Added win counter

--- script.js
+++ script.js
@@ -14,25 +14,31 @@ function playRound(input) {
   if (input == "rock" && computerChoice == "rock") {
     return "Rock and rock tie."
   } else if (input == "rock" && computerChoice == "paper") {
-    return "You lose! Paper covers rock."
+    computerWinCounter++;
+    return "You lose! Paper covers rock.";
   } else if (input == "rock" && computerChoice == "scissors") {
-    return "You win! Rock smashes scissors."
+    playerWinCounter++;
+    return "You win! Rock smashes scissors.";
   }
 
   if (input == "paper" && computerChoice == "rock") {
-    return "You win! Paper covers rock."
+    playerWinCounter++;
+    return "You win! Paper covers rock.";
   } else if (input == "paper" && computerChoice == "paper") {
     return "Paper and paper tie."
   } else if (input == "paper" && computerChoice == "scissors") {
-    return "You lose! Scissors cut paper!"
+    computerWinCounter++;
+    return "You lose! Scissors cut paper!";
   }
 
   if (input == "scissors" && computerChoice == "rock") {
-    return "You lose! Rock smashes scissors!"
+    computerWinCounter++;
+    return "You lose! Rock smashes scissors!";
   } else if (input == "scissors" && computerChoice == "paper") {
-    return "You win! Scissor cut paper!"
+    playerWinCounter++;
+    return "You win! Scissor cut paper!";
   } else if (input == "scissors" && computerChoice == "scissors") {
-    return "Scissors and scissors tie."
+    return "Scissors and scissors tie.";
   }
 }
 
@@ -41,9 +47,12 @@ function game() {
     let input = prompt("Rock, paper, or scissors?")
     input = input.toLocaleLowerCase()
     result = playRound(input)
-    alert(result)
+    alert(result + " " + `Player wins: ${playerWinCounter}, Computer wins: ${computerWinCounter}`)
   }
 }
+
+let playerWinCounter = 0;
+let computerWinCounter = 0;
 
 game()
 
